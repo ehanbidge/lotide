b42f9b7d3cebe1a4b411c802f70249add85f5b9e
ehanbidge <ehanbidge@gmail.com>
2019-12-02 05:22:22 +0000

    Asserting equal objects testing feedback

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,33 +1,68 @@
 
-
-
-const eqObjects = function(object1, object2) {
-
-  let keyList1 = Object.keys(object1);
-  let keyList2 = Object.keys(object2);
-
-  if (keyList1.length !== keyList2.length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   }
-
-  for (let i in object1) {
-    if (typeof(object1[i]) !== typeof(object2[i])){
+  for (let i of array1) {
+    if (array1[i] !== array2[i]) {
       return false;
-    } else if (typeof(object1[i]) === 'object') {
-        if (eqArrays(object1[i], object2[i])) {
-          return true;
-        }
-      return false;
-    } else {
-        if (object1[i] !== object2[i]) {
-        return false;
-        }
     }
   }
   return true;
 };
 
-// FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
-  // Implement me!
+
+  const inspect = require('util').inspect; // <= add this line
+
+  let keyList1 = Object.keys(actual);
+  let keyList2 = Object.keys(expected);
+
+  if (keyList1.length !== keyList2.length) {
+    console.log(`Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+    return;
+  }
+
+  for (let i in actual) {
+    if (typeof(actual[i]) !== typeof(expected[i])){
+      console.log(`Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+      return;
+    } else if (typeof(actual[i]) === 'object') {
+        if (!eqArrays(actual[i], expected[i])) {
+          console.log(`Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+        }
+    } else if (actual[i] !== expected[i]) {
+      console.log(`Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+        return;
+    }
+  }
+  
+  console.log(`${String.fromCodePoint(0x1F600)} Assertion passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+  return true;
 };
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); 
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
+
+
+/*
+const a = { a: "1" };
+assertObjectsEqual(eqObjects(ab, a), false); // => false
+
+const n = {};
+assertObjectsEqual(eqObjects(ab, n), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(eqObjects(cd, cd2), false); // => false
+*/
+
+
